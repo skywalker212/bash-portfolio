e6e20236e3a60f9443762ad4c7c198db483b5213
Akash Gajjar <akashgajjar8@gmail.com>
2024-07-11 11:31:15 -0400

    change configuration files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        terminal: {
+          green: '#00ff00',
+          black: '#000000',
+        },
+      },
+      fontFamily: {
+        mono: ['Courier New', 'monospace'],
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
