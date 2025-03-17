bc9587e78ca72cacdff0b8873608fa3902add7f9
Akash Gajjar <akashgajjar8@gmail.com>
2025-03-16 23:55:59 -0400

    upgrade tailwindcss 😮‍💨 (#10)

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -17,7 +17,7 @@ export default function Error({
         <div className="flex min-h-screen flex-col items-center justify-center">
             <h2 className="text-2xl font-bold">Something went wrong!</h2>
             <button
-                className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-sm hover:bg-blue-600"
                 onClick={() => reset()}
             >
                 Try again
